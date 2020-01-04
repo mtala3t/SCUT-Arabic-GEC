6b6bf55575c475f128a04f3baacdaf8cc34bbb9c
aimanmutasem <aiman_mutasem@hotmail.com>
2020-01-04 14:38:17 +0800

    Delete paths.sh

--- nlp/CS2S+BPE+Emb/paths.sh
+++
@@ -1,7 +0,0 @@
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
-DATA_DIR=$BASE_DIR/data
-MODEL_DIR=$BASE_DIR/models
-SCRIPTS_DIR=$BASE_DIR/scripts
-SOFTWARE_DIR=$BASE_DIR/software
-TRAINING_DIR=$BASE_DIR/training
-
